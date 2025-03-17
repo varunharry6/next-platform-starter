1e9d3e718d95623481e492e2298efac162b9163e
varunharry6 <55380007+varunharry6@users.noreply.github.com>
2025-03-16 21:24:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({ top: targetElement.offsetTop - 50, behavior: "smooth" });
+        });
+    });
+});
